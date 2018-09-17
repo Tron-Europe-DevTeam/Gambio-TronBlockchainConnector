a2eb0b55e7c57fd611b05c4f93dc43e229267fbe
PMD3VSolution <petermuchau@aol.com>
2018-09-18 01:12:08 +0200

    db redesign

--- code/admin/tron-extension/cron/blockchain_sync.php
+++ code/admin/tron-extension/cron/blockchain_sync.php
@@ -22,34 +22,34 @@
    Released under the GNU General Public License
    --------------------------------------------------------------*/
 
-// include external library
-include '/var/www/html/admin/tron-extension/php/inc/global_lib.php';
-include '/var/www/html/admin/tron-extension/php/inc/global_settings.php';
-// set default timezone
-date_default_timezone_set('europe/berlin');
+	// include external library
+	include '/var/www/html/admin/tron-extension/php/inc/global_lib.php';
+	include '/var/www/html/admin/tron-extension/php/inc/global_settings.php';
 
-// vars set to default
-$autosync = 0;
-// init curl connection
-$curlconn = curl_init();
-// create db connection - backend
-$dbconn[0] = dbconnect($dbname[0]);
-// create db connection - gambio
-$dbconn[1] = dbconnect($dbname[1]);
-// read shop address
-$shop_wallet_address = getdbparameter('shopaddress');
+	// set default timezone
+	date_default_timezone_set('europe/berlin');
 
-// check dbconnection
-if(dbconncheck()){
-	mysqli_close($dbconn[0]);
-	mysqli_close($dbconn[1]);
-}
-else {	
-	blockchainsync($dbconn,$curlconn,$shop_wallet_address);
-}
+	//create dbconnection
+	$dbconn = dbconnect($dbname[0]);
+	
+	// check dbconnection
+	if (dbconncheck($dbconn)) {
+		
+		// vars set to default
+		$autosync = 0;	
+		
+		// init curl connection
+		$curlconn = curl_init();	
+		
+		// read shop address
+		$shop_wallet_address = getdbparameter('shopaddress');	
+		
+		// db blockchain sync
+		blockchainsync($dbconn,$curlconn,$shop_wallet_address);	
+		
+		// close request to clear up curl resources
+		curl_close($curlconn);
+		mysqli_close($dbconn);
+	}
 
-// close request to clear up some resources
-    curl_close($curlconn);
-	mysqli_close($dbconn[0]);
-	mysqli_close($dbconn[1]);
 ?>
