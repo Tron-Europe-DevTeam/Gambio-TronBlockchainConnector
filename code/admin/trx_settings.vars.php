8ad37454df5ae513ac20dac4be4d21b6b6c22b07
PMD3VSolution <petermuchau@aol.com>
2018-10-17 06:48:34 +0200

    update patch 1

---
+++ code/admin/trx_settings.vars.php
@@ -0,0 +1,116 @@
+<?php
+/* --------------------------------------------------------------
+   Tron Europe Dev Team
+   Filename: trx_settings_vars.php 
+   
+   15.09.2018 - Init Version
+   
+   Released under the GNU General Public License (Version 2)
+   [http://www.gnu.org/licenses/gpl-2.0.html]
+
+   IMPORTANT! THIS FILE IS DEPRECATED AND WILL BE REPLACED IN THE FUTURE. 
+   MODIFY IT ONLY FOR FIXES. DO NOT APPEND IT WITH NEW FEATURES, USE THE
+   NEW GX-ENGINE LIBRARIES INSTEAD.
+   --------------------------------------------------------------
+
+   based on:
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(orders_status.php,v 1.19 2003/02/06); www.oscommerce.com
+   (c) 2003	 nextcommerce (orders_status.php,v 1.9 2003/08/18); www.nextcommerce.org
+   (c) 2003 XT-Commerce - community made shopping http://www.xt-commerce.com ($Id: orders_status.php 1125 2005-07-28 09:59:44Z novalis $)
+
+   Released under the GNU General Public License
+   --------------------------------------------------------------*/
+	 
+	// Settingsmenue
+	$topic = array( 
+		"0" => array (
+				"title" => "WALLET_SETTINGS",
+				"data" => array (
+					"0" => array (
+							"name"  => "TRON_WALLET_ADDRESS", 	
+							"value" => getdbparameter('shopaddress'),
+							"type"  => "edit",
+							"id"    => "shopaddress"
+					),
+					"1" => array (
+							"name"  => "BLOCKCHAIN_SYNC", 	
+							"value" => getdbparameter('autosync'),
+							"type"  => "switch",
+							"id"    => "autosync"
+					),
+					"2" => array (
+							"name"  => "TBL_ONLY_TRANS_WITH_NOTES", 	
+							"value" => getdbparameter('tblonlytransnote'),
+							"type"  => "switch",
+							"id"    => "tblonlytransnote"
+					)
+				)
+			),
+		"1" => array (
+				"title" => "SYNC_INFOS", 
+				"data" => array (
+					"0" => array (
+							"name"  => "SYNC_TIMESTAMP", 	
+							"value" => getdbparameter('synctime'),
+							"type"  => "edit_disabled",
+							"id"    => "synctime"
+					),					
+					"1" => array (
+							"name"  => "SYNC_TRANSACTIONS", 	
+							"value" => getdbparameter('syncdatacount'),
+							"type"  => "edit_disabled",
+							"id"    => "syncdatacount"
+					),					
+					"2" => array (
+							"name"  => "SYNC_DURATION", 	
+							"value" => getdbparameter('syncduration'),
+							"type"  => "edit_disabled",
+							"id"    => "syncduration"
+					)
+				)
+			),
+		"2" => array (
+				"title" => "BACKEND_SETTINGS", 
+				"data" => array (
+					"0" => array (
+							"name"  => "ORDER_ASSIGNMENT", 	
+							"value" => getdbparameter('ordersync'),
+							"type"  => "switch",
+							"id"    => "ordersync"
+					),
+					"1" => array (
+							"name"  => "WALLET_ASSOCIATION", 	
+							"value" => getdbparameter('walletuserassociation'),
+							"type"  => "switch",
+							"id"    => "walletuserassociation"
+					),
+					"2" => array (
+							"name"  => "ASSIGNMENT_BY_ORDERNUMBER", 	
+							"value" => getdbparameter('assignmentbyordernumber'),
+							"type"  => "switch_disabled",
+							"id"    => "assignmentbyordernumber"
+					),
+					"3" => array (
+							"name"  => "REGEX_ORDERID", 	
+							"value" => getdbparameter('ordernumberregex'),
+							"type"  => "edit",
+							"id"    => "ordernumberregex"
+					),
+					"4" => array (
+							"name"  => "ASSIGNMENT_BY_BILLNUMBER", 	
+							"value" => getdbparameter('assignmentbybillnumber'),
+							"type"  => "switch",
+							"id"    => "assignmentbybillnumber"
+					),
+					"5" => array (
+							"name"  => "REGEX_BILLID", 	
+							"value" => getdbparameter('billnumberregex'),
+							"type"  => "edit",
+							"id"    => "billnumberregex"
+					)
+				)
+			)
+	);
+
+?>
